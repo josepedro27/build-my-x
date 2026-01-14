20652ce8058fc14c38b63bf76b2b8f8185e7ad10
josepedro27 <josepedro_27@hotmail.com>
2026-01-14 11:30:20 +0100

    Add http server

---
+++ 1.Basic_HTTP_server_Python/server.py
@@ -0,0 +1,36 @@
+"""
+Implements a basic HTTP server in Python that responds with "Hello, World!" to any GET request.
+"""
+
+import socket
+
+# Define socket host and port
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = 8000
+
+# Create socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((SERVER_HOST, SERVER_PORT))
+server_socket.listen(1)
+print('Listening on port %s ...' % SERVER_PORT)
+
+while True:    
+    # Wait for client connections
+    client_connection, client_address = server_socket.accept()
+
+    # Get the client request
+    request = client_connection.recv(1024).decode()
+    print(request)
+
+    # Get the content of index.html
+    fin = open('index.html')
+    content = fin.read()
+    fin.close()
+
+    # Send HTTP response
+    response = 'HTTP/1.0 200 OK\n\n' + content
+    client_connection.sendall(response.encode())
+
+# Close socket
+server_socket.close()
